2d6cb7b314aaf9d94307e8011941de549500aac7
Bhargavi-RING <bhargavi.vaidya@kissht.com>
2024-07-23 11:01:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// let localStream;
-// let peerConnection;
-// let socket;
-// let roomId = 'default-room';
+let localStream;
+let peerConnection;
+let socket;
+let roomId = 'default-room';
 
 const configuration = {
     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
 };
 
-// socket = io();
+socket = io('https://65.1.116.84/socket.io/');
 
 async function startCall() {
     try {
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         document.getElementById('localVideo').srcObject = localStream;
 
-    //     peerConnection = new RTCPeerConnection(configuration);
+        peerConnection = new RTCPeerConnection(configuration);
 
-    //     localStream.getTracks().forEach(track => {
-    //         peerConnection.addTrack(track, localStream);
-    //     });
+        localStream.getTracks().forEach(track => {
+            peerConnection.addTrack(track, localStream);
+        });
 
-    //     peerConnection.ontrack = event => {
-    //         if (event.streams && event.streams[0]) {
-    //             document.getElementById('remoteVideo').srcObject = event.streams[0];
-    //         }
-    //     };
+        peerConnection.ontrack = event => {
+            if (event.streams && event.streams[0]) {
+                document.getElementById('remoteVideo').srcObject = event.streams[0];
+            }
+        };
 
-    //     peerConnection.onicecandidate = event => {
-    //         if (event.candidate) {
-    //             socket.emit('ice-candidate', event.candidate, roomId);
-    //         }
-    //     };
+        peerConnection.onicecandidate = event => {
+            if (event.candidate) {
+                socket.emit('ice-candidate', event.candidate, roomId);
+            }
+        };
 
-    //     socket.emit('join-room', roomId);
+        socket.emit('join-room', roomId);
     } 
     catch (error) {
         console.error('Error starting call:', error);
@@ -40,21 +40,21 @@ async function startCall() {
 }
 
 function endCall() {
-    // if (peerConnection) {
-    //     peerConnection.close();
-    // }
-    // if (localStream) {
-    //     localStream.getTracks().forEach(track => track.stop());
-    // }
+    if (peerConnection) {
+        peerConnection.close();
+    }
+    if (localStream) {
+        localStream.getTracks().forEach(track => track.stop());
+    }
     document.getElementById('localVideo').srcObject = null;
     document.getElementById('remoteVideo').srcObject = null;
 }
 
-// socket.on('user-connected', async () => {
-//     const offer = await peerConnection.createOffer();
-//     await peerConnection.setLocalDescription(offer);
-//     socket.emit('offer', offer, roomId);
-// });
+socket.on('user-connected', async () => {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    socket.emit('offer', offer, roomId);
+});
 
 // socket.on('offer', async (offer) => {
 //     await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
@@ -63,17 +63,17 @@ function endCall() {
 //     socket.emit('answer', answer, roomId);
 // });
 
-// socket.on('answer', async (answer) => {
-//     await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
-// });
+socket.on('answer', async (answer) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+});
 
-// socket.on('ice-candidate', async (candidate) => {
-//     try {
-//         await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-//     } catch (error) {
-//         console.error('Error adding ICE candidate:', error);
-//     }
-// });
+socket.on('ice-candidate', async (candidate) => {
+    try {
+        await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+    } catch (error) {
+        console.error('Error adding ICE candidate:', error);
+    }
+});
 
 document.getElementById('startCall').addEventListener('click', startCall);
 document.getElementById('endCall').addEventListener('click', endCall);
