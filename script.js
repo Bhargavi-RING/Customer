e337f8764e18fd16f0febbcca8594e126e5a6581
aakshi-ring <aakshi.vora@kissht.com>
2024-07-25 12:25:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const configuration = {
     iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
 };
 
-socket = io('https://vcip-poc.test.paywithring.com:3000');
+socket = io('https://vcip-poc.test.paywithring.com');
 
 async function startCall() {
     try {
