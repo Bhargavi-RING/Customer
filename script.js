a9c77c57b4cfdd73f749951fd7e11d368babcf94
aakshi-ring <aakshi.vora@kissht.com>
2024-08-01 10:52:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const configuration = {
     { urls: 'stun:stun.l.google.com:19302' },
                 
     {
-        urls: 'turn:vcip-poc.test.paywithring.com:3478',
+        urls: 'turn:vcip-poc.test.paywithring.com',
         username: 'VcipPOC',
         credential: 'TrY123'
     }
